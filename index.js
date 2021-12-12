3e50146136e66592a25098176fec988e1de09275
darksoldier1404 <darksoldier1404@gmail.com>
2021-12-12 13:03:07 +0900

    added SimpleMenu, SimpleAnnouncement and change version check logic

--- index.js
+++ index.js
@@ -3,61 +3,82 @@ const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const http = require('http')
 const request = require('request')
 const fs = require('fs')
-
+const yaml = require('js-yaml')
 bot.once('ready', () => {
-    initStatus();
+    initMessage();
     console.log('DPP is online.');
 });
 
-bot.login(process.env.TOKEN);
-
-let duc = "912173800672612352";
-let dlc = "912169714522947614";
-let dvs = "912171769081786379";
-let dsp = "915392502515773512";
-let die = "915414909993500773";
+// bot.login(process.env.TOKEN);
+bot.login("OTEyMTYwNjk2ODA3MDg0MDgy.YZr52Q.qHM8C8FP04JfWjcHPoB_JEv-ua8");
+let guild;
+let ch = "919421896997621811";
+let vua = "919422966859706369";
+let chm;
 
 // create plugins map
 let plugins = new Map();
-plugins.set(dlc, "DP-LegendaryCash");
-plugins.set(dvs, "DP-VirtualStorage");
-plugins.set(duc, "DP-UniversalCore");
-plugins.set(dsp, "DP-SimplePrefix");
-plugins.set(die, "DP-ItemEditor");
+plugins.set("DP-LegendaryCash", "💵DP-LegendaryCash");
+plugins.set("DP-VirtualStorage", "📦DP-VirtualStorage");
+plugins.set("DP-UniversalCore", "🛠️DP-UniversalCore");
+plugins.set("DP-SimplePrefix", "🏷️DP-SimplePrefix");
+plugins.set("DP-ItemEditor", "🔨DP-ItemEditor");
+plugins.set("DP-SimpleMenu", "🎛️DP-SimpleMenu");
+plugins.set("DP-SimpleAnnouncement", "📢DP-SimpleAnnouncement");
 
-function getPluginVersion(pl) {
-    let guild = bot.guilds.cache.get("740288168716140605");
-    // get file from darksoldier1404 github, repo is DP-LegendaryCash
-    var name = plugins.get(pl);
-    request('https://raw.githubusercontent.com/darksoldier1404/' + name + '/master/src/main/resources/plugin.yml', (error, response, body) => {
-        if (error) {
-            console.error(error)
-            return
-        }
-        if (body) {
-            // body to yaml
-            const yaml = require('js-yaml')
-            const pluginYaml = yaml.load(body)
-                // get version
-            const version = pluginYaml.version
-            const name = pluginYaml.name
-            console.log(name + ' 플러그인 버전 : ' + version);
-            guild.channels.cache.get(pl).setName('Version : ' + version);
-        }
+function initMessage() {
+    guild = bot.guilds.cache.get("740288168716140605");
+    guild.channels.cache.get(ch).send("업데이트 정보 받아오는중...").then(msg => {
+        chm = msg;
+        checkPluginVersions();
+    });
+    setInterval(() => {
+        checkPluginVersions();
+    }, 1000 * 60);
+}
+
+async function checkPluginVersions() {
+    let msg = "";
+    var count = 0;
+    plugins.forEach(async function(title, name) {
+        console.log(name);
+        new Promise(res => {
+            request('https://raw.githubusercontent.com/darksoldier1404/' + name + '/master/src/main/resources/plugin.yml', (error, response, body) => {
+                if (error) {
+                    console.error(error)
+                    count++;
+                    return
+                }
+                if (body) {
+                    // body to yaml
+                    const pluginYaml = yaml.load(body)
+                        // get version
+                    const version = pluginYaml.version
+                    const name = pluginYaml.name
+                    msg += "**" + title + " : " + version + "**\n\n";
+                    count++;
+                }
+                res();
+            })
+        }).then(value => {
+            if (count == plugins.size) {
+                chm.edit(msg);
+            }
+        });
     });
 }
 
-function initStatus() {
-    getPluginVersion(dlc);
-    getPluginVersion(dvs);
-    getPluginVersion(duc);
-    getPluginVersion(dsp);
-    getPluginVersion(die);
-    setInterval(() => {
-        getPluginVersion(dlc);
-        getPluginVersion(dvs);
-        getPluginVersion(duc);
-        getPluginVersion(dsp);
-        getPluginVersion(die);
-    }, 1000 * 60);
-}
+// function initStatus() {
+//     getPluginVersion(dlc);
+//     getPluginVersion(dvs);
+//     getPluginVersion(duc);
+//     getPluginVersion(dsp);
+//     getPluginVersion(die);
+//     setInterval(() => {
+//         getPluginVersion(dlc);
+//         getPluginVersion(dvs);
+//         getPluginVersion(duc);
+//         getPluginVersion(dsp);
+//         getPluginVersion(die);
+//     }, 1000 * 60);
+// }
